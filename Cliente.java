556fa6cd5b2f35fe9229dfd3a61fd71be55e8d1c
Kleberson-Cordeiro <151668684+Kleberson-Cordeiro@users.noreply.github.com>
2024-09-23 12:28:48 -0300

    Cliente.java

---
+++ Cliente.java
@@ -0,0 +1,29 @@
+public class Cliente {
+
+	private String nome;
+	private String cpf;
+	private String endereco;
+	
+	//Gets
+	public String getNome() {
+		return nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public String getendereco() {
+		return endereco;
+	}
+	
+	//Sets
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	
+}
